ad3f0017e56bc88dd214dcccd63f1e94e7e55a5d
Antonio <antonio.fullone@gmail.com>
2019-05-20 13:36:10 +0200

    basic animation for mobile navi

--- src/components/MobileNavigation.js
+++ src/components/MobileNavigation.js
@@ -6,42 +6,41 @@ import bg from '../img/nav-bg.svg';
 import '../styles/mobile-nav.css';
 
 const MobileNavigation = ({ active }) => {
-  const config = { stiffness: 1340, damping: 25 };
-  const toCSS = scale => ({
-    transform: `scale3d(${scale}, ${scale}, ${scale})`,
-    backgroundImage: `url(${bg})`
-  });
   if (active) {
     return (
       <Motion
-        defaultStyle={{ opacity: 0, scale: 0 }}
-        style={{ opacity: spring(1), scale: spring(1, config) }}
+        defaultStyle={{ opacity: 0, x: -600 }}
+        style={{ opacity: spring(1), x: spring(0) }}
       >
         {style => (
           <nav
             className='nav--mobile'
             role='navigation'
             aria-label='main-navigation'
-            style={toCSS(style.scale)}
+            style={{
+              backgroundImage: `url(${bg})`,
+              opacity: style.opacity,
+              transform: `translateX(${style.x}px)`
+            }}
           >
             <ul>
               <li>
-                <Link to=''>Stories</Link>
+                <Link to='/'>Stories</Link>
               </li>
               <li>
-                <Link to=''>Destinations</Link>
+                <Link to='/'>Destinations</Link>
               </li>
               <li>
-                <Link to=''>Countries</Link>
+                <Link to='/'>Countries</Link>
               </li>
               <li>
-                <Link to=''>Expat Tips</Link>
+                <Link to='/'>Expat Tips</Link>
               </li>
               <li>
-                <Link to=''>Travel Tips</Link>
+                <Link to='/'>Travel Tips</Link>
               </li>
               <li>
-                <Link to=''>Guest Posts</Link>
+                <Link to='/'>Guest Posts</Link>
               </li>
             </ul>
           </nav>
