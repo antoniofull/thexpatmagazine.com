c59a9ef0638dabe136a99667d1e5541a9e79ab5b
Antonio <antonio.fullone@gmail.com>
2019-05-25 21:48:44 +0200

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -122,6 +122,7 @@ exports.createPages = ({ actions, graphql }) => {
         createPage({
           path: edge.node.fields.slug,
           tags: edge.node.frontmatter.tags,
+          categories: edge.node.frontmatter.catgory,
           component: path.resolve(
             `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
           ),
