347ee5230f2918b423259d085130862e0da2c67a
Antonio <antonio.fullone@gmail.com>
2019-05-29 17:48:00 +0200

    creating new pages and post template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types';
 import { kebabCase } from 'lodash';
 import Helmet from 'react-helmet';
 import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
 import Layout from '../components/Layout';
 import Content, { HTMLContent } from '../components/Content';
+
+import '../styles/post.css';
 
 export const BlogPostTemplate = ({
   content,
@@ -12,35 +15,40 @@ export const BlogPostTemplate = ({
   description,
   tags,
   title,
+  author,
+  image,
+  category,
   helmet
 }) => {
   const PostContent = contentComponent || Content;
 
   return (
-    <section className='section'>
+    <section className='section post'>
       {helmet || ''}
-      <div className='container content'>
-        <div className='columns'>
-          <div className='column is-10 is-offset-1'>
-            <h1 className='title is-size-2 has-text-weight-bold is-bold-light'>
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className='taglist'>
-                  {tags.map(tag => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-          </div>
+      <Img sizes={image.sizes} className='post__image' />
+      <div className='container post__container'>
+        <h1 className='title is-size-2 has-text-weight-bold is-bold-light'>
+          {title}
+        </h1>
+        <div className='post__meta'>
+          <p className='meta__author'>
+            {author} in {category[0]}
+          </p>
         </div>
+        <p className='post__summary'>{description}</p>
+        <PostContent content={content} />
+        {tags && tags.length ? (
+          <div style={{ marginTop: `4rem` }}>
+            <h4>Tags</h4>
+            <ul className='taglist'>
+              {tags.map(tag => (
+                <li key={tag + `tag`}>
+                  <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : null}
       </div>
     </section>
   );
@@ -63,6 +71,8 @@ const BlogPost = ({ data }) => {
         content={post.html}
         contentComponent={HTMLContent}
         description={post.frontmatter.description}
+        author={post.frontmatter.author}
+        image={post.frontmatter.featuredimage.childImageSharp}
         helmet={
           <Helmet titleTemplate='%s | Blog'>
             <title>{`${post.frontmatter.title}`}</title>
@@ -74,6 +84,7 @@ const BlogPost = ({ data }) => {
         }
         tags={post.frontmatter.tags}
         title={post.frontmatter.title}
+        category={post.frontmatter.category}
       />
     </Layout>
   );
@@ -97,6 +108,15 @@ export const pageQuery = graphql`
         title
         description
         tags
+        category
+        author
+        featuredimage {
+          childImageSharp {
+            sizes(maxWidth: 1200) {
+              ...GatsbyImageSharpSizes
+            }
+          }
+        }
       }
     }
   }
