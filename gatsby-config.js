a94fb031327722e59ecd080289af78e201ce4676
antoniofull <antonio.fullone@gmail.com>
2019-06-14 21:29:05 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,8 +31,6 @@ module.exports = {
                 title
                 description
                 siteURL
-                site_url: siteURL,
-                author
               }
             }
           }
