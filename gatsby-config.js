18a01e60d85e49d312aee2f88c2a50dab8f5428d
antoniofull <antonio.fullone@gmail.com>
2019-06-19 18:47:38 +0200

    zoom images in markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -156,9 +156,11 @@ module.exports = {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 800
+              maxWidth: 800,
+              linkImagesToOriginal: false
             }
           },
+          `gatsby-remark-images-zoom`,
           {
             resolve: 'gatsby-remark-copy-linked-files',
             options: {
