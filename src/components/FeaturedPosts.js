e902e5ee4a7d76af83c39a56dc41c793d39fbd92
antoniofull <antonio.fullone@gmail.com>
2019-08-03 10:01:55 +0200

    clean featured posts

--- src/components/FeaturedPosts.js
+++ src/components/FeaturedPosts.js
@@ -16,34 +16,34 @@ const Featured = ({ post }) => {
       <React.Fragment key={post.node.id}>
         {post.node.frontmatter.featuredimage &&
           post.node.frontmatter.featuredimage.childImageSharp && (
-            <div className='featured-image'>
+            <div className="featured-image">
               <BackgroundImage
-                Tag='section'
-                className='backdrop'
+                Tag="section"
+                className="backdrop"
                 fluid={
                   post.node.frontmatter.featuredimage.childImageSharp.fluid
                 }
               />
             </div>
           )}
-        <div className='featured-post__container has-shadow'>
-          <article className='featured-post'>
-            <header className='featured-post__header'>
+        <div className="featured-post__container has-shadow">
+          <article className="featured-post">
+            <header className="featured-post__header">
               <Link to={post.node.fields.slug}>
-                <h2 className='featured-title'>
+                <h2 className="featured-title">
                   {post.node.frontmatter.title}
                 </h2>
               </Link>
             </header>
-            <div className='meta  meta--featured'>
-              <h3 className='meta--featured__written-by text-color--white wf-os has-horizontal-dividers'>
+            <div className="meta  meta--featured">
+              <h3 className="meta--featured__written-by text-color--white wf-os has-horizontal-dividers">
                 Written by
               </h3>
 
-              <div className='meta--featured__data'>
+              <div className="meta--featured__data">
                 <AuthorMeta author={post.node.frontmatter.author} />
-                <span className='divider--meta' />
-                <span className='meta__date is-text-centered'>
+                <span className="divider--meta" />
+                <span className="meta__date is-text-centered">
                   {post.node.frontmatter.date}
                 </span>
               </div>
@@ -51,7 +51,7 @@ const Featured = ({ post }) => {
 
             <Link
               to={post.node.fields.slug}
-              className='read-more--featured btn btn--primary'
+              className="read-more--featured btn btn--primary"
             >
               Read Full Article
             </Link>
@@ -64,29 +64,29 @@ const Featured = ({ post }) => {
   return null;
 };
 
-const FeaturedPosts = ({ posts, authors, count }) => {
+const FeaturedPosts = ({ posts, authors }) => {
   if (posts && posts.length > 0) {
     // const post = posts[0].node;
     const author = find(authors, a => {
       return a.node.frontmatter.title === posts[0].node.frontmatter.author;
     });
     return (
-      <section className='featured is-color-white'>
-        <div className='featured__container'>
+      <section className="featured is-color-white">
+        <div className="featured__container">
           <Carousel
-            transitionMode='scroll3d'
-            heightMode='max'
+            transitionMode="scroll3d"
+            heightMode="max"
             cellSpacing={0}
-            cellAlign='center'
+            cellAlign="center"
             swiping={true}
             speed={200}
             autoplayInterval={4500}
-            easing='easeCubic'
+            easing="easeCubic"
             dragging={true}
             initialSlideHeight={500}
             enableKeyboardControls={true}
             autoplay={true}
-            width='100%'
+            width="100%"
           >
             {posts.map(post => (
               <Featured key={post.node.id} post={post} author={author} />
