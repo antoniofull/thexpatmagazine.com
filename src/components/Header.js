cca35b945c91f228b176f8f77bdd4577e8389db6
antoniofull <antonio.fullone@gmail.com>
2020-04-26 15:03:25 +0200

    added coil

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import MediaQuery from 'react-responsive';
+import React from 'react'
+import MediaQuery from 'react-responsive'
+import { Helmet } from 'react-helmet'
 
-import Search from './search/';
-import '../styles/header.css';
+import Search from './search/'
+import '../styles/header.css'
 const searchIndices = [
   { name: `Pages`, title: `Pages`, hitComp: `PageHit` },
-  { name: `Posts`, title: `Blog Posts`, hitComp: `PostHit` }
-];
+  { name: `Posts`, title: `Blog Posts`, hitComp: `PostHit` },
+]
 const Header = ({ children }) => (
   <header className='main-header is-color-white' role='banner'>
+    <Helmet>
+      <meta name='monetization' content='$ilp.gatehub.net/429406455' />
+    </Helmet>
     <div className='main-header__container container'>
       {children}
       <MediaQuery query='(min-width: 1024px)'>
@@ -16,6 +20,6 @@ const Header = ({ children }) => (
       </MediaQuery>
     </div>
   </header>
-);
+)
 
-export default Header;
+export default Header
