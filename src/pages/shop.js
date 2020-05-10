436a078edb08b3c8c974b14b3a8109f769a50c76
antoniofull <antonio.fullone@gmail.com>
2020-05-10 09:59:26 +0200

    fix bug for global object in shop

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,33 +1,28 @@
-import React, { useEffect } from 'react'
-
-import Layout from '../components/Layout'
+import React from 'react'
 import Helmet from 'react-helmet'
 
-const Shop = () => {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.addEventListener('DOMCOntentLoaded', () => {
-        window.spread_shop_config = {
-          shopName: 'thexpatmagazine',
-          locale: 'en_GB',
-          prefix: 'https://shop.spreadshirt.co.uk',
-          baseId: 'myShop',
-          startToken: 'the+expat+magazine?idea=5e91b7675fd3e441f18c728f',
-        }
-      })
-    }
-  }, [])
-  return (
-    <Layout>
-      <Helmet>
-        <script
-          type='text/javascript'
-          src='https://shop.spreadshirt.co.uk/shopfiles/shopclient/shopclient.nocache.js'
-        ></script>
-      </Helmet>
-      <div id='myShop'></div>
-    </Layout>
-  )
-}
+import Layout from '../components/Layout'
+
+const Shop = () => (
+  <Layout>
+    <Helmet
+      script={[
+        {
+          type: 'text/javascript',
+          innerHTML: `window.spread_shop_config = { shopName: 'thexpatmagazine', locale: 'en_GB', prefix: 'https://shop.spreadshirt.co.uk', baseId: 'myShop', startToken: 'the+expat+magazine?idea=5e91b7675fd3e441f18c728f'}`,
+        },
+      ]}
+    />
+    <Helmet
+      script={[
+        {
+          src:
+            'https://shop.spreadshirt.co.uk/shopfiles/shopclient/shopclient.nocache.js',
+        },
+      ]}
+    />
+    <div id='myShop'></div>
+  </Layout>
+)
 
 export default Shop
---
+++ src/utils/shop.js
@@ -0,0 +1,7 @@
+export const spread_shop_config = {
+  shopName: 'thexpatmagazine',
+  locale: 'en_GB',
+  prefix: 'https://shop.spreadshirt.co.uk',
+  baseId: 'myShop',
+  startToken: 'the+expat+magazine?idea=5e91b7675fd3e441f18c728f',
+}
