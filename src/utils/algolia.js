163ecc291cd909df5f6b784cc197b82a00a2a2d1
antoniofull <antonio.fullone@gmail.com>
2020-05-10 10:19:04 +0200

    remove unused file, update algolia search data

--- src/utils/algolia.js
+++ src/utils/algolia.js
@@ -19,12 +19,10 @@ const pageQuery = `{
         }
       }
     }
-  }`;
+  }`
 
 const postQuery = `{
-    posts: allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "/blog/" } }
-    ) {
+    posts: allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/blog/"}}) {
       edges {
         node {
           objectID: id
@@ -35,33 +33,34 @@ const postQuery = `{
             title
             date(formatString: "MMM D, YYYY")
             tags
+            author
           }
-          excerpt(pruneLength: 5000)
+          excerpt(pruneLength: 200)
         }
       }
     }
-  }`;
+  }`
 
-const flatten = arr =>
+const flatten = (arr) =>
   arr.map(({ node: { frontmatter, ...rest } }) => ({
     ...frontmatter,
-    ...rest
-  }));
-const settings = { attributesToSnippet: [`excerpt:20`] };
+    ...rest,
+  }))
+const settings = { attributesToSnippet: [`excerpt:20`] }
 
 const queries = [
   {
     query: pageQuery,
     transformer: ({ data }) => flatten(data.pages.edges),
     indexName: `Pages`,
-    settings
+    settings,
   },
   {
     query: postQuery,
     transformer: ({ data }) => flatten(data.posts.edges),
     indexName: `Posts`,
-    settings
-  }
-];
+    settings,
+  },
+]
 
-module.exports = queries;
+module.exports = queries
