9d90131060ad2d65a642fa138e539e8d1b6a7215
antoniofull <antonio.fullone@gmail.com>
2021-01-27 19:09:56 +0100

    fix wrong image missing

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,42 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import Helmet from 'react-helmet';
-import { graphql } from 'gatsby';
-import Content, { HTMLContent } from '../components/Content';
+import React, { useState, useEffect } from 'react'
+import Helmet from 'react-helmet'
+import { graphql } from 'gatsby'
+import Content, { HTMLContent } from '../components/Content'
 import { GatsbySeo } from 'gatsby-plugin-next-seo'
-import Layout from '../components/Layout';
-import '../styles/page.css';
-import {BASE_URL} from '../utils/utils'
-import kebabCase from 'lodash/kebabCase';
+import Layout from '../components/Layout'
+import '../styles/page.css'
+import { BASE_URL } from '../utils/utils'
+import kebabCase from 'lodash/kebabCase'
 
 const PageRoute = ({ data }) => {
   const [img, setImg] = useState('as')
-  const { title, description } = data.markdownRemark.frontmatter;
-  const PageContent = HTMLContent || Content;
+  const { title, description } = data.markdownRemark.frontmatter
+  const PageContent = HTMLContent || Content
   useEffect(() => {
-    if(typeof window !== undefined && window.document) {
+    if (
+      typeof window !== undefined &&
+      window.document &&
+      window.document.querySelector('img')
+    ) {
       setImg(window.document.querySelector('img').getAttribute('src'))
-    } 
+    }
   }, [data])
   return (
     <Layout>
       <GatsbySeo
-          language='en'
-          title={title}
-           description={description}
-           openGraph={{
-            url: `${BASE_URL}${kebabCase(title)}`,
-            title: title,
-            description: description,
-            images: [
-              {
-                url: img,
-                width: 800,
-                height: 600,
-                alt: title,
-              },
-            ],
-          }}
-        />
+        language='en'
+        title={title}
+        description={description}
+        openGraph={{
+          url: `${BASE_URL}${kebabCase(title)}`,
+          title: title,
+          description: description,
+          images: [
+            {
+              url: img,
+              width: 800,
+              height: 600,
+              alt: title
+            }
+          ]
+        }}
+      />
       <section className='container page'>
         <Helmet title={title} />
         <h1 className='post__title'>{title}</h1>
@@ -46,10 +50,10 @@ const PageRoute = ({ data }) => {
         />
       </section>
     </Layout>
-  );
-};
+  )
+}
 
-export default PageRoute;
+export default PageRoute
 
 export const PageQuery = graphql`
   query PageData($id: String!) {
@@ -62,4 +66,4 @@ export const PageQuery = graphql`
       }
     }
   }
-`;
+`
